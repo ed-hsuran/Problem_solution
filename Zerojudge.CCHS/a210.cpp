e53be696851298b5f7a478c5c5eb39b71dfcf0c9
Ed_hsuran <106414128+ed-hsuran@users.noreply.github.com>
2022-05-30 11:39:41 +0800

    Update a210.cpp

--- Zerojudge.CCHS/a210.cpp
+++ Zerojudge.CCHS/a210.cpp
@@ -40,7 +40,7 @@ vector<int> dijkstra(vector <pii> adj[], int n, int v){
 
 int main(){
 	ios_base::sync_with_stdio(false);
-	cin.tie(0);
+	cin.tie(NULL);
 	
 	int n, m, s, e;
 	cin >> n >> m >> s >> e;
