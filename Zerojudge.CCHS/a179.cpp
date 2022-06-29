b942894c18107b5704a0f7d42ea0a2675c8f9681
Ed_hsuran <106414128+ed-hsuran@users.noreply.github.com>
2022-06-29 18:37:36 +0800

    Update a179.cpp

--- Zerojudge.CCHS/a179.cpp
+++ Zerojudge.CCHS/a179.cpp
@@ -4,17 +4,16 @@ using namespace std;
 /*
 滑窗法
 用Map儲存當前窗格的彩色顏色與數量
-把目前map.size()紀錄，與長度L相等，答案++
-滑窗還是不用開Queue
+把目前彩帶數量用map.size()紀錄，與長度L相等，答案++
 */
 
-int L, n, c = 0;
-int arr[200005];
+int L, n, c = 0, s;
+queue <int> q;
 unordered_map <int, int> mp;
 
 void add(int x, int y = -1){
-	mp[x]++;
-	if (y > -1) mp[y]--;
+	mp[x]++; q.push(x);
+	if (y > -1) mp[y]--, q.pop();
 	if (!mp[y]) mp.erase(mp.find(y));
 	if (L == (int)mp.size()) c++;
 }
@@ -24,8 +23,7 @@ int main(){
 	cin.tie(0);
 	
 	cin >> L >> n;
-	for (int i=0; i<n; i++) cin >> arr[i];
-	for (int i=0; i<L; i++) add(arr[i]);
-	for (int i=L; i<n; i++) add(arr[i], arr[i-L]);
+	for (int i=0; i<L; i++) cin >> s, add(s);
+	for (int i=L; i<n; i++) cin >> s, add(s, q.front());
 	cout << c;
 }
